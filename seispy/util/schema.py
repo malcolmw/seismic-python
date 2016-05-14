d4b6eaec4f3b21ef5e37498f629d684aa55aac4a
Malcolm White <mcwhite@ucsd.edu>
2016-05-13 17:23:18 -0700

    Working on schema parsing.

--- seispy/util/schema.py
+++ seispy/util/schema.py
@@ -1,17 +1,15 @@
 '''
 A container class for database schema definitions.
 '''
+from inspect import getmembers
 from os.path import isfile
 from seispy.core.exceptions import InitializationError
-_lead_tokens = ('(', '{', '"', "'", 'Attribute', 'Detail', 'Relation', 'Schema')
+_lead_tokens = ('(', '{', '"')
 _token_conjugates = {'(': ')',
                      '{': '}',
                      '"': '"',
-                     "'": "'",
-                     'Attribute': ';',
-                     'Detail': ';',
-                     'Relation': ';',
-                     'Schema': ';'}
+                     "'": "'"}
+_attribute_dtypes = ('String', 'Integer', 'Real')
 
 def find_token(string, ttype=None):
     '''
@@ -34,10 +32,29 @@ def find_token(string, ttype=None):
                 return char, i, _token_conjugates[char]
     return None, None
 
-
-class Attribute:
+class DatabaseElement:
     def __init__(self, *args, **kwargs):
-        pass
+        for key in kwargs:
+            setattr(self, key, kwargs[key])
+
+    def __str__(self):
+        keys = []
+        s = ''
+        for key, value in getmembers(self):
+            if len(key) > 2 and key[:2] == '__':
+                continue
+            if hasattr(value, '__call__'):
+                continue
+            keys += [key]
+        s = ''
+        width = max([len(k) for k in keys])
+        for key in keys:
+            s += "{:>{width}}:".format(key, width=width)
+            s += "\t{}\n".format(getattr(self, key))
+        return s
+
+class Attribute(DatabaseElement):
+    pass
 
 class AttributeParseError:
     def __init__(self, message):
@@ -46,36 +63,89 @@ class AttributeParseError:
     def __str__(self):
         return self.message
 
+class Relation(DatabaseElement):
+    def __str__(self):
+        s = self.name + "\n"
+        s += "-" * len(self.name)
+        width = max([len(attr) for attr in self.attributes])
+        for attr in self.attributes:
+            s += "{:>{width}}\n".format(attr, width=width)
+        return s
+
 class Schema:
     def __init__(self, path):
+        self.attributes = {}
+        self.relations = {}
         self.token_parser = TokenParser(path)
         self.parse_header()
+        self.parse_body()
+
+    def __str__(self):
+        s = ''
+        for relation in self.relations:
+            s += "{}\n".format(self.relations[relation])
+        return s
+
+    def parse_body(self):
+        tp = self.token_parser
+        block = tp.get_block()
+        while tp.get_block():
+            tp.get_field()
+            #print tp.get_key(), tp.get_value()
+            if tp.get_key() == 'Attribute':
+                print "self.parse_attribute()"
+                self.parse_attribute()
+            elif tp.get_key() == 'Relation':
+                print "self.parse_relation()"
+                self.parse_relation()
+            else:
+                raise Exception("Error code 3000: unrecognized specifier "\
+                        "{:s}".format(tp.get_key()))
 
     def parse_attribute(self):
-        while True:
-            line = self.infile.readline().strip()
-            if line == '': continue
-            spec, value = line.split()
-            token, pos, conj = find_token(value)
-            if pos == None:
-                raise AttributeParseError("no lead token found while parsing "\
-                        "Attribute")
-            if conj == None:
-                pass
-            value = value[pos + 1:]
-            token, pos, conj = find_token(value, ttype=conj)
+        tp = self.token_parser
+        kwargs = {}
+        name = tp.get_value()
+        kwargs['name'] = name
+        while tp.get_field():
+            key = tp.get_key()
+            if name == 'net':
+                print name, key, tp.get_value()
+            if key in _attribute_dtypes:
+                kwargs['dtype'] = key
+                kwargs['width'] = tp.get_value()
+            else:
+                kwargs[key] = tp.get_value()
+        self.attributes[name] = Attribute(**kwargs)
+
+    def parse_relation(self):
+        tp = self.token_parser
+        kwargs = {}
+        name = tp.get_value()
+        kwargs['name'] = name
+        while tp.get_field():
+            kwargs[tp.get_key()] = tp.get_value()
+        self.relations[name] = Relation(**kwargs)
 
     def parse_header(self):
-        if not token_parser.get_chunk() == 'Schema':
+        tp = self.token_parser
+        tp.get_block()
+        tp.get_field()
+        if tp.get_key() != 'Schema':
             raise Exception("Error code 1000")
-        schema_header = TokenParser(token_parser.get_chunk())
-        self.schema = schema_header.get_chunk()
-        if not schema_header.get_chunk() == 'Description':
+        self.schema = tp.get_value()
+        field = tp.get_field()
+        if tp.get_key() != 'Description':
             raise Exception("Error code 1001")
-        desc_block = schema_header.get_chunk()
-        if not schema_header.get_chunk() == 'Detail':
+        self.description= tp.get_value()
+        tp.get_field()
+        if tp.get_key() != 'Detail':
             raise Exception("Error code 1002")
-        det_block = schema_header.get_chunk()
+        self.detail = tp.get_value()
+        tp.get_field()
+        if tp.get_key() != 'Timedate':
+            raise Exception("Error code 1003")
+        self.timedate = tp.get_value()
 
 class TokenParser:
     def __init__(self, *args):
@@ -87,59 +157,61 @@ class TokenParser:
             self.stream = args[0]
         else:
             raise InitializationError("unrecognized argument type")
-        self.token = None
-        self.chunk = ''
 
-    def get_chunk(self):
-        self.chunk = self.next()
-        if self.chunk == '':
-            print "get_chunk(1) ==> None"
+    def get_block(self):
+        tokens = []
+        char = self.next()
+        if char == None:
             return None
-        elif self.chunk == None:
-            print "get_chunk(2) ==> None"
+        self.block = char
+        while char != ';' or tokens:
+            char = self.next()
+            #print "!{}$".format(char)
+            if char == None:
+                self.block = None
+                return None
+            if tokens and char == tokens[-1]:
+                tokens.pop()
+            elif char in _lead_tokens:
+                tokens += [_token_conjugates[char]]
+            self.block += char
+        self.block_cursor = 0
+        return self.block
+
+    def get_field(self):
+        self.field = ''
+        tokens = []
+        while self.block_cursor < len(self.block):
+            char = self.block[self.block_cursor]
+            self.block_cursor += 1
+            if tokens and char == tokens[-1]:
+                tokens.pop()
+            elif char in _lead_tokens:
+                tokens += [_token_conjugates[char]]
+            if not tokens and char == '\n':
+                if self.field == '':
+                    self.get_field()
+                return self.field
+            self.field += char
+        self.field = None
+        return None
+
+    def get_key(self):
+        if self.field == None:
             return None
-        elif not self.token == '}'  and not self.token == ';'\
-                and (self.chunk == '\n' or self.chunk == '\t'):
-            while self.chunk == '\n' or self.chunk == '\t':
-                print "get_chunk(3) ==> while..."
-                self.chunk = self.next()
-        #Need to check the value of self.token here...
-        elif self.chunk == ' ' or self.chunk == '\t' or self.chunk in _lead_tokens:
-            while self.chunk == ' ' or self.chunk == '\t' or self.chunk in _lead_tokens:
-                print "get_chunk(4) ==> while... {:s}".format(self.chunk)
-                self.chunk = self.next()
-                if self.chunk in _lead_tokens:
-                    print "get_chunk(4) ==> break... {:s}".format(self.chunk)
-                    break
-        if self.chunk in _lead_tokens:
-            self.token = _token_conjugates[self.chunk]
-            print "get_chunk(5) ==> {:s}".format(self.chunk)
-            return self.chunk
-        while True:
-            c = self.next()
-            if c == None:
-                print "get_chunk(6) ==> {:s}".format(self.chunk)
-                return self.chunk
-            self.chunk += c
-            if self.token:
-                if c == self.token:
-                    self.token = None
-                    print "get_chunk(7) ==> {:s}".format(self.chunk[:-1].rstrip().lstrip())
-                    return self.chunk[:-1].rstrip().lstrip()
-            else:
-                if c == '\n':
-                    self.token = None
-                    self.chunk += c
-                    print "get_chunk(8) ==> {:s}".format(self.chunk.rstrip().lstrip())
-                    return self.chunk.rstrip().lstrip()
-                elif c in _lead_tokens:
-                    self.token = _token_conjugates[c]
-                    print "get_chunk(9) ==> {:s}".format(self.chunk.rstrip().lstrip())
-                    return self.chunk[:-1].rstrip().lstrip()
-                elif self.chunk in _lead_tokens:
-                    print "get_chunk(10) ==> {:s}".format(self.chunk.rstrip().lstrip())
-                    self.token = _token_conjugates[self.chunk]
-                    return self.chunk.rstrip().lstrip()
+        return self.field.strip().split()[0]
+
+    def get_value(self):
+        if self.field == None:
+            return None
+        field = self.field.lstrip()
+        i = 0
+        while field[i] != ' ':
+            i += 1
+        val = field[i:].strip()
+        if val[0] in _lead_tokens and val[-1] == _token_conjugates[val[0]]:
+            val = val[1:-1]
+        return val.strip()
 
     def next(self):
         if self.type == file:
@@ -147,8 +219,6 @@ class TokenParser:
             if c == '':
                 return None
             else:
-                if c == '':
-                    return None
                 return c
         else:
             if len(self.stream) == 0:
