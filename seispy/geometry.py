74dea1e219353f5c8365910fb9b94a025a92dd13
Malcolm White <malcolcw@usc.edu>
2016-09-12 18:15:06 -0700

    Add functionality to plot vertical slices for constant latitude.

--- seispy/geometry.py
+++ seispy/geometry.py
@@ -25,7 +25,13 @@ CARTESIAN
 Let's align our cartesian coordinates so the x-axis corresponds to the
 lon = 0 axis and the z-axis aligns with North pole lat=90.
 """
-from math import pi
+from math import acos,\
+                 atan2,\
+                 cos,\
+                 pi,\
+                 sin,\
+                 sqrt
+import numpy as np
 
 EARTH_RADIUS = 6371.
 
@@ -50,3 +56,52 @@ def geo2sph(lat, lon, z):
     phi = deg2rad(lon)
     r = EARTH_RADIUS - z
     return r, theta, phi
+
+def sph2xyz(r, theta, phi):
+    x = r * sin(theta) * cos(phi)
+    y = r * sin(theta) * sin(phi)
+    z = r * cos(theta)
+    return x, y, z
+
+def xyz2sph(x, y, z):
+    r = sqrt(x ** 2 + y ** 2 + z ** 2)
+    theta = acos(z / r)
+    phi = atan2(y, x)
+    return r, theta, phi
+
+def rotation_matrix(axis, theta):
+    """
+    Return the rotation matrix associated with counterclockwise rotation about
+    the given axis by theta radians.
+    """
+    if axis == 1 or axis == 'x' or axis == 'X':
+        return np.array([[1, 0, 0],
+                         [0, cos(theta), -sin(theta)],
+                         [0, sin(theta), cos(theta)]])
+    elif axis == 2 or axis == 'y' or axis == 'Y':
+        return np.array([[cos(theta), 0, sin(theta)],
+                         [0, 1, 0],
+                         [-sin(theta), 0, cos(theta)]])
+    elif axis == 3 or axis == 'z' or axis == 'Z':
+        return np.array([[cos(theta), -sin(theta), 0],
+                         [sin(theta), cos(theta), 0],
+                         [0, 0, 1]])
+    else:
+        raise ValueError("invalid axis")
+
+class Vector(np.ndarray):
+
+    def __new__(cls, v):
+        u = np.ndarray.__new__(cls, len(v))
+        for i in range(len(v)):
+            u[i] = v[i]
+        return u
+
+    def rotate(self, axis, theta):
+        return self.__new__(self.__class__, rotation_matrix(axis, theta).dot(self))
+
+    def translate(self, v):
+        u = self.__new__(self.__class__, [0 for i in range(len(v))])
+        for i in range(len(v)):
+            u[i] = self[i] - v[i]
+        return u
--- seispy/velocity.py
+++ seispy/velocity.py
@@ -1,8 +1,19 @@
+from math import cos,\
+                 degrees,\
+                 pi,\
+                 radians,\
+                 sin
+import matplotlib.pyplot as plt
+from matplotlib.mlab import griddata
+
+from numpy import linspace,\
+                  zeros
 #from seispy.geometry import deg2rad,\
-from geometry import deg2rad,\
-                            EARTH_RADIUS,\
-                            geo2sph
-from numpy import zeros
+from geometry import EARTH_RADIUS,\
+                            geo2sph,\
+                            sph2xyz,\
+                            Vector
+import geometry as geom
 
 class VelocityModel(object):
     """
@@ -28,8 +39,8 @@ class VelocityModel(object):
         lat_nodes = [float(lat) for lat in infile.readline().split()]
         z_nodes = [float(z) for z in infile.readline().split()]
         r_nodes = [EARTH_RADIUS - z for z in z_nodes]
-        theta_nodes = [deg2rad(90. - lat) for lat in lat_nodes]
-        phi_nodes = [deg2rad(lon) for lon in lon_nodes]
+        theta_nodes = [radians(90. - lat) for lat in lat_nodes]
+        phi_nodes = [radians(lon) for lon in lon_nodes]
         self.model = {
             'Vp': zeros((len(r_nodes), len(theta_nodes), len(phi_nodes))),
             'Vs': zeros((len(r_nodes), len(theta_nodes), len(phi_nodes)))
@@ -43,6 +54,18 @@ class VelocityModel(object):
                     self.nodes[ir, itheta, iphi] = (r_nodes[ir],
                                                     theta_nodes[itheta],
                                                     phi_nodes[iphi])
+        self.r_max = max([n[0] for n in self.nodes[:, 0, 0]])
+        self.r_min = min([n[0] for n in self.nodes[:, 0, 0]])
+        self.r_mid = (self.r_max + self.r_min) / 2.
+        self.dr = (self.r_max - self.r_min) / len(r_nodes)
+        self.theta_max = max([n[1] for n in self.nodes[0, :, 0]])
+        self.theta_min = min([n[1] for n in self.nodes[0, :, 0]])
+        self.theta_mid = (self.theta_max + self.theta_min) / 2.
+        self.dtheta = (self.theta_max - self.theta_min) / len(theta_nodes)
+        self.phi_max = max([n[2] for n in self.nodes[0, 0, :]])
+        self.phi_min = min([n[2] for n in self.nodes[0, 0, :]])
+        self.phi_mid = (self.phi_max + self.phi_min) / 2.
+        self.dphi = (self.phi_max - self.phi_min) / len(phi_nodes)
         #initialize the grids of velocity values
         for phase in ('Vp', 'Vs'):
             for ir in range(len(r_nodes)):
@@ -69,14 +92,14 @@ class VelocityModel(object):
                 itheta0 = itheta
                 break
         for iphi in range(self.nodes.shape[2] - 1):
-            if self.nodes[0, 0, iphi][2] > phi > self.nodes[0, 0, phi + 1][2]:
+            if self.nodes[0, 0, iphi][2] < phi < self.nodes[0, 0, iphi + 1][2]:
                 iphi0 = iphi
                 break
-        if not ir0:
+        if ir0 == None:
             ir0 = [n[0] for n in self.nodes[:, 0, 0]].index(r)
-        if not itheta0:
+        if itheta0 == None:
             itheta0 = [n[1] for n in self.nodes[0, :, 0]].index(theta)
-        if not iphi0:
+        if iphi0 == None:
             iphi0 = [n[2] for n in self.nodes[0, 0, :]].index(phi)
         #still need to deal with edges properly
         #do a trilinear interpolation
@@ -116,9 +139,12 @@ class VelocityModel(object):
         return self._get_V(r, theta, phi, 'Vp')
 
     def get_Vs(self):
-        pass
+        r, theta, phi = geo2sph(lat, lon, depth)
+        return self._get_V(r, theta, phi, 'Vs')
 
     def _plot(self, lat, lon, depth, phase):
+        NPTS = 100
+        X, Y, V = [], [], []
         if lat == -999 and lon == -999:
             #plot depth slice
             pass
@@ -126,11 +152,38 @@ class VelocityModel(object):
             #plot longitudinal slice
             pass
         elif lon == -999 and depth == -999:
-            #plot latitudinal slice
-            pass
+            #get data points
+            theta = radians(90 - lat)
+            r_nodes = linspace(self.r_min + 0.01 * self.dr,
+                               self.r_max - 0.01 * self.dr,
+                               NPTS)
+            phi_nodes = linspace(self.phi_min + 0.01 * self.dphi,
+                                 self.phi_max - 0.01 * self.dphi,
+                                 NPTS)
+            T = sph2xyz(self.r_min, theta, self.phi_mid)
+            for r in r_nodes:
+                for phi in phi_nodes:
+                    X += [r * cos(pi / 2 - (phi - self.phi_mid))]
+                    Y += [r * sin(pi / 2 - (phi - self.phi_mid)) - self.r_max]
+                    V += [self._get_V(r, theta, phi, phase)]
+        x_min, x_max = min(X), max(X)
+        y_min, y_max = min(Y), max(Y)
+        Xi = linspace(x_min, x_max, NPTS)
+        Yi = linspace(y_min, y_max, NPTS)
+        Vi = griddata(X, Y, V, Xi, Yi)[-1::-1]
+        fig = plt.figure()
+        ax = fig.add_subplot(1, 1, 1)
+        ax.imshow(Vi,
+                  extent=(x_min, x_max, y_min, y_max),
+                  aspect=(x_max - x_min) / (y_max - y_min) * 0.5)
+        #ax = fig.add_subplot(2, 1, 2)
+        #ax.tripcolor(X, Y, V)
+        #ax.plot(X, Y, 'k.')
+        plt.show()
+        
 
     def plot_Vp(self, lat=-999, lon=-999, depth=-999):
-        pass
+        self._plot(lat, lon, depth, 'Vp')
 
     def plot_Vs(self):
         pass
