20b3faab8dda18e594ebaba7eeb9d15ea463c71a
Malcolm White <malcolcw@usc.edu>
2016-10-03 17:09:36 -0700

    Add fault-zone head-wave detection functionality.

--- seispy/core.py
+++ seispy/core.py
@@ -7,7 +7,9 @@ import obspy
 from obspy.core import read,\
                        Stream
 from obspy.core.utcdatetime import UTCDateTime
+from scipy.signal import argrelmin, argrelmax
 
+from seispy.signal.statistics import pai_s, pai_k, f90trigger
 from seispy.signal.detect import detectS as _detectS_cc
 from seispy.signal.detect import create_polarization_filter
 from seispy.util import validate_time
@@ -443,6 +445,332 @@ class Trace(obspy.core.Trace):
         super(self.__class__, self).filter(*args, **kwargs)
         self.trim(starttime=self.stats.starttime + 5)
 
+    def noise_pad(self, twin, noise_twin=0):
+        if noise_twin == 0:
+            #Draw random noise.
+            pass
+            return
+        noise = self.data[:noise_twin * self.stats.sampling_rate]
+        npad_samples= twin * self.stats.sampling_rate
+        self.trim(starttime=self.stats.starttime - twin, pad=True, fill_value=0.0)
+        self.data[:npad_samples] = np.random.choice(noise, size=npad_samples, replace=True)
+
+    def pick_fzhw(self,
+                  q_thr=0.03,
+                  dt_min=0.065,
+                  max_vc=0.10,
+                  vp=5.5,
+                  snr=None,
+                  kurt=None,
+                  skew=None,
+                  polar_flip=True):
+        """
+        Automatic picker for fault zone head waves. Only requires a single
+        component (usually vertical) for usage. The picker makes an initial
+        pick for the earliest onset of particle motion, and then makes two
+        more in an attempt to find a direct P wave. If the initial pick and
+        secondary detectors are too close in time, no fzhw is picked and only
+        a direct P is identified.
+        picks
+        :type fault: float64 or [float64]*4
+        :param fault: Station-fault distance or start-end coordinates of fault
+        :type q_thr: float64
+        :param q_thr: Maximum allowable time difference between P picks
+        :type dt_min: float64
+        :param dt_min: Minim allowed time separation
+        :type max_vc: float64
+        :param max_vc: maximum velocity contrast allowed
+        :type vp: float64
+        :param vp: P wave velocity (km/s) to assume for contrast calculation
+        :type para: list of dictionaries
+        :param para: Contains picker params for each picker
+        :type detector: bool
+        :param detector: Return CF traces on exit?
+        :return: [picks, Final_P_pick, dt offset, CF traces (optional)]
+        If a hw is not present, 'Final_P_pick' = None
+        """
+        SNR_par= {'on': 5,
+                  'off': 4,
+                  'sta': 0.1,
+                  'lta': 10,
+                  'peak_min': 0,
+                  'dur_min': 0}
+
+        if snr is not None:
+            for key in snr:
+                SNR_par[key] = snr[key] 
+
+        K_par = {'on': 15,
+                 'off': 2,
+                 'wlen': 5,
+                 'peak_min': 0}
+
+        if kurt is not None:
+            for key in kurt:
+                K_par[key] = kurt[key]
+
+        S_par = {'on': 3,
+                 'off': 0.5,
+                 'wlen': 5,
+                 'peak_min': 0}
+
+        if skew is not None:
+            for key in skew:
+                S_par[key] = skew[key]
+
+        hw_pick, hw_alt, cft1 = self._first_pick(SNR_par)
+        K_pick, K_alt, cft2 = self._K_pick(K_par)
+        S_pick, S_alt, cft3 = self._S_pick(S_par)
+
+        # Check that picks were made properly
+        if hw_pick is None:
+            return None
+        head_wave = Detection(self.stats.station,
+                              self.stats.channel,
+                              self.stats.starttime + hw_pick,
+                              'H')
+        if K_pick is None or S_pick is None:
+            return [head_wave]
+        if np.mean([K_pick, S_pick]) < hw_pick:
+            return [head_wave]
+        cft = np.column_stack((cft1, cft2, cft3))
+
+        dt = []
+        dt.append(K_pick - hw_pick)
+        dt.append(S_pick - hw_pick)
+
+        fs = self.stats.sampling_rate
+        hw_pick = int(hw_pick*fs)
+        hw_alt = int(hw_alt*fs)
+        K_pick = int(K_pick*fs)
+        K_alt = int(K_alt*fs)
+        S_pick = int(S_pick*fs)
+        S_alt = int(S_alt*fs) 
+
+        # Check for polarity reversal near skewness gradient peak
+        half_width = S_alt - S_pick
+        start = S_pick - half_width
+        stop = S_pick + half_width
+        skew_win = cft3[start:stop]
+        zeros = np.where(np.diff(np.sign(skew_win)))[0] + 1
+
+        if zeros.size == 0:
+            return [head_wave]
+        zeros += start
+        p_flip = zeros[np.argmin(np.abs(zeros-S_pick))]
+        P_pol = np.sign(cft3[p_flip])
+
+        # Check for polarity reversal near hw pick (error checking)
+        start = hw_pick - int(0.02*fs)
+        stop = hw_pick + int(0.02*fs)
+        hw_win = cft3[start:stop]
+        zeros = np.where(np.diff(np.sign(hw_win)))[0] + 1
+        if zeros.size == 0:
+            HW_pol = np.sign(cft3[hw_pick])
+            hw_flip = hw_pick
+        elif zeros.size == 1:
+            zeros += start
+            hw_flip = zeros[np.argmin(np.abs(zeros-hw_pick))]
+            HW_pol = np.sign(cft3[hw_flip])
+        else:
+            zeros += start
+            hw_flip = zeros[-1]
+            HW_pol = np.sign(cft3[hw_flip])
+        if polar_flip:
+            if HW_pol == P_pol:
+                return [head_wave]
+
+        # Make sure the skewness polarity doesn't flip multiple times
+        hw_win = cft3[hw_flip+1:p_flip]
+        zeros = np.where(np.diff(np.sign(hw_win)))[0] + 1
+        if zeros.size > 0:
+            return [head_wave]
+
+        # Check that P picks are in specific range from HW pick
+        vp_slow = vp - max_vc*vp
+        dt_max = self.fzhw_p_dt(vp, vp_slow)
+        if dt_max <= 0:
+            return [head_wave]
+
+        # Check that time differences are in allowed range
+        if dt[0] < dt_min or dt[1] < dt_min:
+            return [head_wave]
+        if dt[0] > dt_max or dt[1] > dt_max:
+            return [head_wave]
+        # Check that picks are nearly coincident
+        if np.abs(K_pick - S_pick) / fs  >= q_thr:
+            return [head_wave]
+
+        # Refine skewness pick with extrema near polarity flip
+        minima = argrelmin(cft3[p_flip-1*half_width:p_flip+1])[0]
+        if len(minima) >= 1:
+            min_pick = minima[len(minima)-1] + p_flip - 1*half_width
+        maxima = argrelmax(cft3[p_flip-1*half_width:p_flip+1])[0]
+        if len(maxima) >= 1:
+            max_pick = maxima[len(maxima)-1] + p_flip - 1*half_width
+        if len(maxima) >= 1 and len(minima) >= 1:
+            if max_pick > min_pick:
+                S_pick = max_pick
+            else:
+                S_pick = min_pick
+        elif len(maxima) >= 1:
+            S_pick = max_pick
+        elif len(minima) >= 1:
+            S_pick = min_pick
+
+        # Refining kurtosis pick with minima near polarity flip
+        minima = argrelmin(cft2[p_flip-1*half_width:p_flip+1])[0]
+        if len(minima) >= 1:
+            K_pick = minima[len(minima)-1] + p_flip - 1*half_width
+        hw_pick = hw_alt
+        P_pick = (K_pick+S_pick)/2
+
+        # Algorithm is concluded; mark first motion as FZHW
+        return [Detection(self.stats.stations,
+                          self.stats.channel,
+                          self.stats.starttime + P_pick * self.stats.delta,
+                          'P'),
+                head_wave]
+
+    def _first_pick(self, par):
+        """
+        'trace' is an ObsPy trace class containing a vertical cmp seismogram
+        'par' is a dict containing the following quantities:
+        'dur_min' is the minimum duration for trigger window
+        'peak_min' is the minimum SNR value allowed for the peak STA/LTA
+        'sta': short term average window length in seconds
+        'lta': long term average window length in seconds
+        'on': STA/LTA value for trigger window to turn on
+        'off': STA/LTA value for trigger window to turn off
+        """
+        dur_min = par['dur_min']
+        peak_min = par['peak_min']
+        sta = par['sta']
+        lta = par['lta']
+        on = par['on']
+        off = par['off']
+
+        # Calculate characteristic function
+        picks = []
+        fs = self.stats.sampling_rate
+        cft = self.copy()
+        cft.trigger('recstalta', sta=sta, lta=lta)
+        triggers = _trigger_onset(cft.data, on, off)
+        for on_t, off_t in triggers:
+            if off_t <= on_t:
+                continue
+            if (off_t - on_t)/fs < dur_min:
+                continue
+            MAX = np.max(cft.data[on_t:off_t])
+            idx = np.argmax(cft.data[on_t:off_t])
+            if MAX >= peak_min:
+                picks.append((on_t, MAX))
+
+        # Take the largest peak value
+        if not picks:
+            return None, None, None
+        else:
+            p_pick = max(picks, key=lambda x: x[1])[0]
+            SNR = max(picks, key=lambda x: x[1])[1]
+
+        start = p_pick - int(0.03*fs)
+        alt_pick = np.argmin(cft.data[start:p_pick]) + start
+
+        return p_pick/fs, alt_pick/fs, cft.data
+
+    def _K_pick(self, par):
+        """
+        Make a pick for sharpest arrival using kurtosis
+        'trace' is an ObsPy trace class containing a vertical cmp seismogram
+        'par' is a dict with the following keys:
+        'peak_min' is the minimum K value allowed for the peak
+        'wlen': sliding window length in seconds
+        'on': K value for trigger window to turn on
+        'off': K value for trigger window to turn off
+        """
+        peak_min = par['peak_min']
+        wlen = par['wlen']
+        on = par['on']
+        off = par['off']
+
+        # Calculate characteristic function
+        picks = []
+        fs = self.stats.sampling_rate
+        cft = self.copy()
+        cft.data = pai_k(cft.data, int(wlen*fs))
+
+        triggers = _trigger_onset(cft.data, on, off)
+        for on_t, off_t in triggers:
+            if off_t <= on_t:
+                continue
+            MAX = np.max(cft.data[on_t:off_t])
+            idx = np.argmax(cft.data[on_t:off_t])
+            if MAX >= peak_min:
+                picks.append((on_t+idx, MAX))
+
+        # Take the largest peak value
+        if not picks:
+            return None, None, None
+        else:
+            k_pick = max(picks, key=lambda x: x[1])[0]
+
+        # Refining pick using nearby derivative
+        alt_pick = k_pick
+        start = k_pick - int(fs)
+        grad = np.gradient(cft.data, 1/fs)
+        if start <= 0:
+            return None, None, None
+        k_pick = np.argmax(grad[start:k_pick]) + start - 1
+        return k_pick/fs, alt_pick/fs, cft.data
+
+    def _S_pick(self, par):
+        """
+        Make a pick for sharpest arrival using skewness
+        'trace' is an ObsPy trace class containing a vertical cmp seismogram
+        'par' is a dict with the following keys:
+        'peak_min' is the minimum S value allowed for the peak
+        'wlen': sliding window length in seconds
+        'on': S value for trigger window to turn on
+        'off': S value for trigger window to turn off
+        """
+        peak_min = par['peak_min']
+        wlen = par['wlen']
+        on = par['on']
+        off = par['off']
+
+        # Calculate characteristic function
+        picks = []
+        fs = self.stats.sampling_rate
+        cft_clean = self.copy()
+        cft_clean.data = pai_s(cft_clean.data, int(wlen*fs))
+        cft = cft_clean.copy()
+        cft.data = np.abs(cft.data)
+
+        triggers = _trigger_onset(cft.data, on, off)
+        for on_t, off_t in triggers:
+            if off_t <= on_t:
+                continue
+            MAX = np.max(cft.data[on_t:off_t])
+            idx = np.argmax(cft.data[on_t:off_t])
+            if MAX >= peak_min:
+                picks.append((on_t+idx, MAX))
+
+        # Take the largest peak value
+        if not picks:
+            return None, None, None
+        else:
+            s_pick = max(picks, key=lambda x: x[1])[0]
+
+        # Refining pick using nearby derivative
+        alt_pick = s_pick
+        start = s_pick - int(fs)
+        grad = np.gradient(cft.data, 1/fs)
+        if start <= 0:
+            return None, None, None
+        s_pick = np.argmax(grad[start:s_pick]) + start - 1
+        return s_pick/fs, alt_pick/fs, cft.data
+        
+
     def plot(self,
              starttime=None,
              endtime=None,
@@ -551,3 +879,12 @@ def _order_orthogonal_channels(channels):
             channel2, channel3 = channels[1], channels[2]
         channels = (channels[0], channel2, channel3)
     return channels
+
+def _trigger_onset(cft, on, off):
+    res = f90trigger(cft, on, off)
+    ons = res[0]
+    offs = res[1]
+    idx = np.nonzero(ons)[0]
+    ons = ons[idx]
+    offs = offs[idx]
+    return [list(a) for a in zip(ons, offs)] 
--- seispy/signal/statistics.f90
+++ seispy/signal/statistics.f90
@@ -1,79 +1,31 @@
 !Statistical signal processing routines
 
 ! ----------------------------------------
-subroutine cov_filter(Z, N, E, r, phi, npts, wlen)
-    ! Recursive sliding covariance matrix calculation
-    implicit none
+subroutine pai_s(trace, output, M, npts)
+  ! PAI-S Skewness function defined by Saragiotis et al. (2002)
+  implicit none
 
-    ! Argument declarations
-    double precision, intent(in) :: Z(npts), E(npts), N(npts)
-    double precision, intent(out) :: r(npts), phi(npts)
-    double precision :: C(3,3), W(3), work(8)
-    double precision :: CNN(npts), CEE(npts), CZZ(npts)
-    double precision :: CNE(npts), CNZ(npts), CZE(npts)
-    integer :: npts, info, i, wlen, lwork
+  ! Argument declarations
+  integer :: npts, M
+  double precision, intent(in) :: trace(npts)
+  double precision, intent(out) :: output(npts)
 
-    ! Initialize variables & matrices
-    r(1:npts) = 0
-    phi(1:npts) = 1
-    lwork = 8
+  ! Variable declarations
+  double precision :: numer, denom, X(M), mean, diff(M) 
+  integer :: i
 
-    r(1:wlen) = 0
-    phi(1:wlen) = 0
+  output = 0
+  mean = sum(trace(1:M))
+  do i = M, npts 
+    X = trace(i-M+1:i)
+    mean = mean - trace(i-M+1) + trace(i+1)
+    diff = (X-mean/M)
+    numer = sum(diff**3)/M
+    denom = (sum(diff**2)/(M-1))**(3./2.)
+    output(i) = numer/denom
+  end do
 
-    call moving_cov(N, N, CNN, npts, wlen)
-    call moving_cov(E, E, CEE, npts, wlen)
-    call moving_cov(Z, Z, CZZ, npts, wlen)
-    call moving_cov(N, Z, CNZ, npts, wlen)
-    call moving_cov(N, E, CNE, npts, wlen)
-    call moving_cov(Z, E, CZE, npts, wlen)
-
-    do i = wlen+1, npts
-        ! Store result in matrix for given time index
-        C(1,1) = CEE(i)
-        C(2,2) = CNN(i)
-        C(3,3) = CZZ(i)
-        C(1,2) = CNE(i)
-        C(1,3) = CZE(i)
-        C(2,3) = CNZ(i)
-        C(3,2) = C(2,3)
-        C(3,1) = C(1,3)
-        C(2,1) = C(1,2)
-
-        ! Calculate eigenvectors
-        call DSYEV('V', 'U', 3, C, 3, W, work, lwork, info)
-
-        ! Store polarization quantities
-        r(i) = 1 - (W(1)+W(2))/(2*W(3))
-        phi(i) = abs(C(3,3))
-    end do
-
-end subroutine cov_filter
-
-! ----------------------------------------
-subroutine moving_cov(X, Y, C, npts, wlen)
-    implicit none
-    double precision, intent(in) :: X(npts), Y(npts)
-    double precision, intent(out) :: C(npts)
-    double precision :: xm, ym, xm_old, ym_old, cov
-    integer, intent(in) :: wlen
-    integer :: npts, den, i
-
-    xm = sum(X(1:wlen))
-    ym = sum(Y(1:wlen))
-    cov = dot_product(X(1:wlen)-xm/wlen, Y(1:wlen)-ym/wlen)
-    den = npts*npts
-    C(1:wlen) = 0
-    do i = wlen+1, npts
-        xm_old = xm
-        ym_old = ym
-        xm = xm + X(i) - X(i-wlen)
-        ym = ym + Y(i) - Y(i-wlen)
-        cov = cov + X(i)*Y(i) - X(i-wlen)*Y(i-wlen) !+ npts*(xm_old*ym_old/den -xm*ym/den) 
-        C(i) = cov
-    end do
-
-end subroutine moving_cov
+end subroutine PAI_S
 
 ! ----------------------------------------
 subroutine pai_k(trace, output, M, npts)
@@ -104,109 +56,10 @@ subroutine pai_k(trace, output, M, npts)
 end subroutine PAI_K
 
 ! ----------------------------------------
-subroutine pai_s(trace, output, M, npts)
-  ! PAI-S Skewness function defined by Saragiotis et al. (2002)
-  implicit none
-
-  ! Argument declarations
-  integer :: npts, M
-  double precision, intent(in) :: trace(npts)
-  double precision, intent(out) :: output(npts)
-
-  ! Variable declarations
-  double precision :: numer, denom, X(M), mean, diff(M)
-  integer :: i
-
-  output = 0
-  mean = sum(trace(1:M))
-  do i = M, npts
-    X = trace(i-M+1:i)
-    mean = mean - trace(i-M+1) + trace(i+1)
-    diff = (X-mean/M)
-    numer = sum(diff**3)/M
-    denom = (sum(diff**2)/(M-1))**(3./2.)
-    output(i) = numer/denom
-  end do
-
-end subroutine PAI_S
-
-! ----------------------------------------
-subroutine snr(trace, cft, nsta, nlta, npts, on, off)
-
-    ! recursive mean calculation
-    implicit none
-
-    ! Argument declarations
-    integer :: npts
-    double precision, intent(in) :: trace(npts), on, off
-    integer, intent(in) :: nsta, nlta
-    double precision, intent(out) :: cft(npts)
-
-    ! Variable declarations
-    double precision :: sta, lta, X(nsta), Y(nlta)
-    integer :: state, i
-    state = 0
-    cft(1:nlta) = 0
-    do i = nlta, npts
-        X = trace(i-nsta+1:i)
-        Y = trace(i-nlta+1:i)
-        sta = sum(X**2)/nsta
-        if (state == 0) then
-            lta = sum(Y**2)/nlta
-            cft(i) = sta/lta
-            if (cft(i) >= on) then
-                state = 1
-            end if
-        else
-            cft(i) = sta/lta
-            if (cft(i) < off) then
-                state = 0
-            end if
-        end if
-    end do
-
-end subroutine snr
-
-! ----------------------------------------
-subroutine stalta(trace, cft, nsta, nlta, npts)
-
-    ! recursive mean calculation
-    implicit none
-
-    ! Argument declarations
-    integer :: npts
-    double precision, intent(in) :: trace(npts), nsta, nlta
-    double precision, intent(out) :: cft(npts)
-
-    ! Variable declarations
-    double precision :: alpha, beta, inv_alpha, inv_beta, square
-    double precision :: sta, lta
-    integer :: i
-
-    alpha = 1/nsta
-    beta = 1/nlta
-    inv_alpha = 1 - alpha
-    inv_beta = 1 - beta
-    sta = 0
-    lta = 1e-30
-
-    do i = 2, npts
-        square = trace(i)**2
-        sta = alpha*square + inv_alpha*sta
-        lta = beta*square + inv_beta*lta
-        cft(i) = sta/lta
-        if (i <= nlta) then
-            cft(i) = 0
-        end if
-    end do
-
-end subroutine stalta
-
-! ----------------------------------------
-subroutine trigger(x, t_on, t_off, N, ntrig, on, off)
+subroutine f90trigger(x, t_on, t_off, N, ntrig, on, off)
   implicit none
   double precision, intent(in) :: x(N), on, off
-  integer, intent(out) :: t_on(N), t_off(N), ntrig
+  integer, intent(out) :: t_on(N), t_off(N), ntrig 
   integer, intent(in) :: N
   integer :: i, mode
 
@@ -231,5 +84,4 @@ subroutine trigger(x, t_on, t_off, N, ntrig, on, off)
     t_off(ntrig) = N - 1
   end if
 
-end subroutine trigger
-
+end subroutine f90trigger
