44755fbe2d3437e771858e38cb428f32dbf979ac
Malcolm White <malcolcw@usc.edu>
2017-03-30 17:13:03 -0700

    Fix import statements.

--- seispy/__init__.py
+++ seispy/__init__.py
@@ -1,2 +1,6 @@
-import geometry
-import ttgrid
+import importlib
+
+submodules = ["geometry",
+              "ttgrid"]
+for submodule in submodules:
+    importlib.import_module(".{}".format(submodule), package="seispy")
