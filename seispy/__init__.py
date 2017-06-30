51f680c782b41ef4b71bb377dfe951cb5dab9ecd
Malcolm White <malcolcw@usc.edu>
2017-06-30 15:18:31 -0700

    Add Topography class
    Add Topography class to handle surface elevation data.

--- seispy/__init__.py
+++ seispy/__init__.py
@@ -1,6 +1,7 @@
 import importlib
 
 submodules = ["geometry",
+              "topography",
               "ttgrid"]
 for submodule in submodules:
     importlib.import_module(".{}".format(submodule), package="seispy")
--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
 import setuptools
 # Initialize the setup kwargs that are independent of Antelope.
-kwargs = {"name": "seismicpython",
+kwargs = {"name": "seismic-python",
           "version": "1.0a0",
           "author": "Malcolm White",
           "author_email": "malcolcw@usc.edu",
           "maintainer": "Malcolm White",
           "maintainer_email": "malcolcw@usc.edu",
-          "url": "http://malcolmw.github.io/seismicpython",
+          "url": "http://malcolmw.github.io/seismic-python",
           "description": "Seismic data analysis tools",
-          "download_url": "https://github.com/malcolmw/seismicpython.git",
+          "download_url": "https://github.com/malcolmw/seismic-python.git",
           "platforms": ["linux", "osx"],
-          "requires": ["obspy"],
+          "requires": ["obspy", "sqlite"],
           "py_modules": ["seispy.geometry",
+                         "seispy.topography",
                          "seispy.ttgrid"]}
 
 kwargs["packages"] = ["seispy"]
