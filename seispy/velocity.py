4f0a338bb352c152b4de5b3f3ab385b35e7a429f
Malcolm White <malcolcw@usc.edu>
2017-07-03 14:24:21 -0700

    DEBUG
    DEBUG
    
    
    DEBUG
    
    
    DEBUG
    
    
    CLEANUP

--- seispy/velocity.py
+++ seispy/velocity.py
@@ -66,7 +66,9 @@ class VelocityModel(object):
         # Make sure 0 <= phi < 2*pi
         phi %= 2 * pi
         if r > self.topo(theta, phi):
-            raise(ValueError("point above surface"))
+            # Return P-wave velocity in air if requested point is above
+            # surface.
+            return(0.323)
         r = max(r, self.nodes["r_min"])
         theta = min(max(theta, self.nodes["theta_min"]),
                     self.nodes["theta_max"])
@@ -116,19 +118,19 @@ class VelocityModel(object):
 
     def print_interfaces(self, propgrid, stretch=1.01, size_ratio=2, basement=30):
         igrid = _calculate_grid_parameters(propgrid)
-        nlat, nlon = igrid["nlat"], igrid["nlon"]
-        dlat, dlon = radians(igrid["dlat"]), radians(igrid["dlon"])
-        lat0, lon0 = radians(igrid["lat0"]), radians(igrid["lon0"])
+        ntheta, nphi = igrid["ntheta"], igrid["nphi"]
+        dtheta, dphi = radians(igrid["dtheta"]), radians(igrid["dphi"])
+        theta0, phi0 = radians(igrid["theta0"]), radians(igrid["phi0"])
         blob = "2\n"\
                "{:d} {:d}\n"\
                "{:.4f} {:.4f}\n"\
-               "{:.4f} {:.4f}\n".format(nlat, nlon,
-                                        dlat, dlon,
-                                        lat0, lon0)
+               "{:.4f} {:.4f}\n".format(ntheta, nphi,
+                                        dtheta, dphi,
+                                        theta0, phi0)
         for r in (seispy.constants.EARTH_RADIUS + 5,
                   seispy.constants.EARTH_RADIUS - basement):
-            for ilat, ilon in [(ilat, ilon) for ilat in range(nlat)
-                                            for ilon in range(nlon)]:
+            for itheta, iphi in [(itheta, iphi) for itheta in range(ntheta)
+                                                for iphi in range(nphi)]:
                 blob += "{:.4f}\n".format(r)
         return(blob.rstrip())
 
@@ -138,56 +140,55 @@ class VelocityModel(object):
                "{:.6f} {:.6f} {:.6f}\n"\
                "5 10".format(grid["nr"], grid["nlat"], grid["nlon"],
                              grid["dr"], grid["dlat"], grid["dlon"],
-                             grid["h0"], grid["lat0"], grid["lon0"]))
+                             grid["h0"], grid["lat0"], grid["lon0"] % 360))
 
     def print_vgrid(self, phase, propgrid, stretch=1.01, size_ratio=2):
         phase = _verify_phase(phase)
         vgrid = _calculate_grid_parameters(propgrid)
-        nr, nlat, nlon = vgrid["nr"], vgrid["nlat"], vgrid["nlon"]
-        dr, dlat, dlon = vgrid["dr"], radians(vgrid["dlat"]), radians(vgrid["dlon"])
-        r0, lat0, lon0 = vgrid["r0"], radians(vgrid["lat0"]), radians(vgrid["lon0"])
+        nr, ntheta, nphi = vgrid["nr"], vgrid["ntheta"], vgrid["nphi"]
+        dr, dtheta, dphi = vgrid["dr"], radians(vgrid["dtheta"]), radians(vgrid["dphi"])
+        r0, theta0, phi0 = vgrid["r0"], radians(vgrid["theta0"]), radians(vgrid["phi0"])
         blob = "{:d} {:d} {:d}\n"\
                "{:.4f} {:.4f} {:.4f}\n"\
-               "{:.4f} {:.4f} {:.4f}\n".format(nr, nlat, nlon,
-                                               dr, dlat, dlon,
-                                               r0, lat0, lon0)
-        for (ir, ilat, ilon) in [(ir, ilat, ilon) for ir in range(nr)\
-                                                  for ilat in range(nlat)\
-                                                  for ilon in range(nlon)]:
-            lat = degrees(lat0 + dlat * ilat)
-            lon = degrees(lon0 + dlon * ilon) % 360.
+               "{:.4f} {:.4f} {:.4f}\n".format(nr, ntheta, nphi,
+                                               dr, dtheta, dphi,
+                                               r0, theta0, phi0)
+        for (ir, itheta, iphi) in [(ir, itheta, iphi) for ir in range(nr)\
+                                                  for itheta in range(ntheta)\
+                                                  for iphi in range(nphi)]:
+            lat = degrees(theta0 + dtheta * itheta)
+            lon = degrees(phi0 + dphi * iphi) % 360.
             r = r0 + dr * ir
             depth = seispy.constants.EARTH_RADIUS - r
             blob += "{:f}\n".format(self(phase, lat, lon, depth))
         return(blob.rstrip())
 
 def _calculate_grid_parameters(grid, stretch=1.01, size_ratio=2):
-        pnr, pnlat, pnlon = grid["nr"], grid["nlat"], grid["nlon"]
-        pdr, pdlat, pdlon = grid["dr"], radians(grid["dlat"]), radians(grid["dlon"])
-        ph0, plat0, plon0 = grid["h0"], radians(grid["lat0"]), radians(grid["lon0"])
+        pnr, pntheta, pnphi = grid["nr"], grid["nlat"], grid["nlon"]
+        pdr, pdtheta, pdphi = grid["dr"], radians(grid["dlat"]), radians(grid["dlon"])
         pr0, ptheta0, pphi0 = seispy.geometry.geo2sph(grid["lat0"],
                                                       grid["lon0"],
                                                       -grid["h0"])
-        plat0, plon0 = ptheta0, pphi0
-        pr0 = seispy.constants.EARTH_RADIUS + ph0 - ((pnr - 1) * pdr)
+    #    plat0, plon0 = ptheta0, pphi0
+    #    pr0 = seispy.constants.EARTH_RADIUS + ph0 - ((pnr - 1) * pdr)
         i = (pnr - 1) % size_ratio
         pnr = pnr + (size_ratio - i) if i > 0 else pnr
-        i = (pnlat - 1) % size_ratio
-        pnlat = pnlat + (size_ratio - i) if i > 0 else pnlat
-        i = (pnlon - 1) % size_ratio
-        pnlon = pnlon + (size_ratio - i) if i > 0 else pnlon
+        i = (pntheta - 1) % size_ratio
+        pntheta = pntheta + (size_ratio - i) if i > 0 else pntheta
+        i = (pnphi - 1) % size_ratio
+        pnphi = pnphi + (size_ratio - i) if i > 0 else pnphi
         nr = int((pnr - 1) / size_ratio) + 3
-        nlat = int((pnlat - 1) / size_ratio) + 3
-        nlon = int((pnlon - 1) / size_ratio) + 3
+        ntheta = int((pntheta - 1) / size_ratio) + 3
+        nphi = int((pnphi - 1) / size_ratio) + 3
         dr = stretch * pdr * size_ratio
-        dlat = stretch * pdlat * size_ratio
-        dlon = stretch * pdlon * size_ratio
+        dtheta = stretch * pdtheta * size_ratio
+        dphi = stretch * pdphi * size_ratio
         r0 = pr0 - dr - (nr - 1) * dr * (stretch - 1) / 2
-        lat0 = plat0 - dlat - (nlat - 1) * dlat * (stretch - 1) / 2
-        lon0 = plon0 - dlon - (nlon - 1) * dlon * (stretch - 1) / 2
-        return({"nr": nr, "nlat": nlat, "nlon": nlon,
-                "dr": dr, "dlat": dlat, "dlon": dlon,
-                "r0": r0, "lat0": lat0, "lon0": lon0})
+        theta0 = ptheta0 - dtheta - (ntheta - 1) * dtheta * (stretch - 1) / 2
+        phi0 = pphi0 - dphi - (nphi - 1) * dphi * (stretch - 1) / 2
+        return({"nr": nr, "ntheta": ntheta, "nphi": nphi,
+                "dr": dr, "dtheta": dtheta, "dphi": dphi,
+                "r0": r0, "theta0": theta0, "phi0": phi0})
 
 def _verify_phase(phase):
     if phase.upper() == "P" or  phase.upper() == "VP":
