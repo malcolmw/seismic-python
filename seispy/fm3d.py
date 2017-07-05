6e7e321bf13bcc648ce77640210b4780279e758c
Malcolm White <malcolcw@usc.edu>
2017-07-05 08:57:58 -0700

    DEBUG 2017-07-03
    DEBUG
    
    
    DEBUG
    
    
    DEBUG
    
    
    DEBUG
    
    
    DEBUG
    
    
    DEBUG
    
    
    DEBUG
    
    
    DEBUG
    
    
    DEBUG
    
    
    DEBUG
    
    
    DEBUG
    
    
    DEBUG
    
    
    DEBUG
    
    
    DEBUG
    
    
    DEBUG
    
    
    DEBUG
    
    
    DEBUG
    
    
    DEBUG
    
    
    DEBUG
    
    
    DEBUG
    
    
    DEBUG
    
    
    DEBUG
    
    
    DEBUG
    
    
    DEBUG
    
    
    DEBUG
    
    
    DEBUG
    
    
    DEBUG
    
    
    DEBUG
    
    
    DEBUG
    
    
    DEBUG
    
    
    DEBUG
    
    
    DEBUG
    
    
    DEBUG
    
    
    DEBUG

--- seispy/fm3d.py
+++ seispy/fm3d.py
@@ -1,33 +1,65 @@
-def print_fm3d(vm, phase, propgrid, stretch=1.01, size_ratio=2, basement=30):
-    return(vm.print_propgrid(propgrid),
-           vm.print_vgrid(phase,
-                          propgrid,
-                          stretch=stretch,
-                          size_ratio=2),
-           vm.print_interfaces(propgrid,
-                               stretch=stretch,
-                               size_ratio=size_ratio,
-                               basement=basement))
+from math import degrees,\
+                 pi,\
+                 radians
+import seispy
 
-def print_interfaces(vm, propgrid, stretch=1.01, size_ratio=2, basement=30):
+def print_fm3d(vm, phase, propgrid, stretch=1.01, size_ratio=2, basement=30):
+    return(print_propgrid(propgrid),
+           print_vgrid(vm,
+                       phase,
+                       propgrid,
+                       stretch=stretch,
+                       size_ratio=2),
+           print_interfaces(propgrid,
+                            stretch=stretch,
+                            size_ratio=size_ratio,
+                            basement=basement))
+
+def print_interfaces(propgrid, stretch=1.01, size_ratio=2, basement=30):
     igrid = _calculate_grid_parameters(propgrid)
-    ntheta, nphi = igrid["ntheta"], igrid["nphi"]
-    dtheta, dphi = radians(igrid["dtheta"]), radians(igrid["dphi"])
-    theta0, phi0 = radians(igrid["theta0"]), radians(igrid["phi0"])
+    nlat, nlon = igrid["nlat"], igrid["nlon"]
+    dlat, dlon = igrid["dlat"], igrid["dlon"]
+    lat0, lon0 = igrid["lat0"], igrid["lon0"]
     blob = "2\n"\
            "{:d} {:d}\n"\
            "{:.4f} {:.4f}\n"\
-           "{:.4f} {:.4f}\n".format(ntheta, nphi,
-                                    dtheta, dphi,
-                                    theta0, phi0)
-    for r in (seispy.constants.EARTH_RADIUS + 5,
-              seispy.constants.EARTH_RADIUS - basement):
-        for itheta, iphi in [(itheta, iphi) for itheta in range(ntheta)
-                                            for iphi in range(nphi)]:
+           "{:.4f} {:.4f}\n".format(nlat, nlon,
+                                    dlat, dlon,
+                                    lat0, lon0)
+    top = seispy.constants.EARTH_RADIUS + propgrid["h0"]
+    bottom = seispy.constants.EARTH_RADIUS + propgrid["h0"] -\
+            (propgrid["nr"] - 1) * propgrid["dr"]
+    for r in (top, bottom):
+        for ilat, ilon in [(ilat, ilon) for ilat in range(nlat)
+                                        for ilon in range(nlon)]:
             blob += "{:.4f}\n".format(r)
     return(blob.rstrip())
 
-def print_propgrid(vm, grid):
+def print_mode_set(file_mode=False,
+                   no_pp_mode=False,
+                   parallel_mode=False,
+                   display_mode=False,
+                   save_rays_mode=False,
+                   save_timefields_mode=False):
+    file_mode = "T" if file_mode is True else "F"
+    no_pp_mode = "T" if no_pp_mode is True else "F"
+    parallel_mode = "T" if parallel_mode is True else "F"
+    display_mode = "T" if display_mode is True else "F"
+    save_rays_mode = "T" if save_rays_mode is True else "F"
+    save_timefields_mode = "T" if save_timefields_mode is True else "F"
+    return("{}\tfile_mode\n"\
+           "{}\tno_pp_mode\n"\
+           "{}\tparallel_mode\n"\
+           "{}\tdisplay_mode\n"\
+           "{}\tsave_rays_mode\n"\
+           "{}\tsave_timefields_modes".format(file_mode,
+                                              no_pp_mode,
+                                              parallel_mode,
+                                              display_mode,
+                                              save_rays_mode,
+                                              save_timefields_mode))
+
+def print_propgrid(grid):
     return("{:d} {:d} {:d}\n"\
            "{:.6f} {:.6f} {:.6f}\n"\
            "{:.6f} {:.6f} {:.6f}\n"\
@@ -37,47 +69,46 @@ def print_propgrid(vm, grid):
 
 def print_vgrid(vm, phase, propgrid, stretch=1.01, size_ratio=2):
     vgrid = _calculate_grid_parameters(propgrid)
-    nr, ntheta, nphi = vgrid["nr"], vgrid["ntheta"], vgrid["nphi"]
-    dr, dtheta, dphi = vgrid["dr"], radians(vgrid["dtheta"]), radians(vgrid["dphi"])
-    r0, theta0, phi0 = vgrid["r0"], radians(vgrid["theta0"]), radians(vgrid["phi0"])
-    blob = "{:d} {:d} {:d}\n"\
-            "{:.4f} {:.4f} {:.4f}\n"\
-            "{:.4f} {:.4f} {:.4f}\n".format(nr, ntheta, nphi,
-                                            dr, dtheta, dphi,
-                                            r0, theta0, phi0)
-    for (ir, itheta, iphi) in [(ir, itheta, iphi) for ir in range(nr)\
-                                                  for itheta in range(ntheta)\
-                                                  for iphi in range(nphi)]:
-        lat = degrees(theta0 + dtheta * itheta)
-        lon = degrees(phi0 + dphi * iphi) % 360.
+    nr, nlat, nlon = vgrid["nr"], vgrid["nlat"], vgrid["nlon"]
+    dr, dlat, dlon = vgrid["dr"], vgrid["dlat"], vgrid["dlon"]
+    r0, lat0, lon0 = vgrid["r0"], vgrid["lat0"], vgrid["lon0"]
+    blob = "1 1\n"\
+           "{:d} {:d} {:d}\n"\
+           "{:.4f} {:.4f} {:.4f}\n"\
+           "{:.4f} {:.4f} {:.4f}\n".format(nr, nlat, nlon,
+                                           dr, dlat, dlon,
+                                           r0, lat0, lon0)
+    for (ir, ilat, ilon) in [(ir, ilat, ilon) for ir in range(nr)\
+                                                  for ilat in range(nlat)\
+                                                  for ilon in range(nlon)]:
+        lat = degrees(lat0 + dlat * ilat)
+        lon = degrees(lon0 + dlon * ilon) % 360.
         r = r0 + dr * ir
         depth = seispy.constants.EARTH_RADIUS - r
         blob += "{:f}\n".format(vm(phase, lat, lon, depth))
     return(blob.rstrip())
 
 def _calculate_grid_parameters(grid, stretch=1.01, size_ratio=2):
-    pnr, pntheta, pnphi = grid["nr"], grid["nlat"], grid["nlon"]
-    pdr, pdtheta, pdphi = grid["dr"], radians(grid["dlat"]), radians(grid["dlon"])
-    pr0, ptheta0, pphi0 = seispy.geometry.geo2sph(grid["lat0"],
-                                                    grid["lon0"],
-                                                    -grid["h0"])
-#    plat0, plon0 = ptheta0, pphi0
-#    pr0 = seispy.constants.EARTH_RADIUS + ph0 - ((pnr - 1) * pdr)
+    pnr, pnlat, pnlon = grid["nr"], grid["nlat"], grid["nlon"]
+    pdr, pdlat, pdlon = grid["dr"], radians(grid["dlat"]), radians(grid["dlon"])
+    ph0, plat0, plon0 = grid["h0"], radians(grid["lat0"]), radians(grid["lon0"])
+    plon0 %= 2 * pi
+    pr0 = seispy.geometry.EARTH_RADIUS + ph0 - ((pnr - 1) * pdr)
     i = (pnr - 1) % size_ratio
     pnr = pnr + (size_ratio - i) if i > 0 else pnr
-    i = (pntheta - 1) % size_ratio
-    pntheta = pntheta + (size_ratio - i) if i > 0 else pntheta
-    i = (pnphi - 1) % size_ratio
-    pnphi = pnphi + (size_ratio - i) if i > 0 else pnphi
+    i = (pnlat - 1) % size_ratio
+    pnlat = pnlat + (size_ratio - i) if i > 0 else pnlat
+    i = (pnlon - 1) % size_ratio
+    pnlon = pnlon + (size_ratio - i) if i > 0 else pnlon
     nr = int((pnr - 1) / size_ratio) + 3
-    ntheta = int((pntheta - 1) / size_ratio) + 3
-    nphi = int((pnphi - 1) / size_ratio) + 3
+    nlat = int((pnlat - 1) / size_ratio) + 3
+    nlon = int((pnlon - 1) / size_ratio) + 3
     dr = stretch * pdr * size_ratio
-    dtheta = stretch * pdtheta * size_ratio
-    dphi = stretch * pdphi * size_ratio
-    r0 = pr0 - dr - (nr - 1) * dr * (stretch - 1) / 2
-    theta0 = ptheta0 - dtheta - (ntheta - 1) * dtheta * (stretch - 1) / 2
-    phi0 = pphi0 - dphi - (nphi - 1) * dphi * (stretch - 1) / 2
-    return({"nr": nr, "ntheta": ntheta, "nphi": nphi,
-            "dr": dr, "dtheta": dtheta, "dphi": dphi,
-            "r0": r0, "theta0": theta0, "phi0": phi0})
+    dlat = stretch * pdlat * size_ratio
+    dlon = stretch * pdlon * size_ratio
+    r0 = pr0 - dr - (nr - 1) * dr * (stretch - 1.0) / 2
+    lat0 = plat0 - dlat - (nlat - 1) * dlat * (stretch - 1.0) / 2
+    lon0 = plon0 - dlon - (nlon - 1) * dlon * (stretch - 1.0) / 2
+    return({"nr": nr, "nlat": nlat, "nlon": nlon,
+            "dr": dr, "dlat": dlat, "dlon": dlon,
+            "r0": r0, "lat0": lat0, "lon0": lon0})
