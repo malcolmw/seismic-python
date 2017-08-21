e1c396a71eed68e657e020417dc1cfadfa5b524f
Malcolm White <malcolcw@usc.edu>
2017-08-21 15:39:06 -0700

    Fix upside-down velocity model and add some ray-tracer I/O functions

--- seispy/propagate.py
+++ seispy/propagate.py
@@ -1,20 +1,18 @@
 import numpy as np
 import seispy
-import velocity
 import fm3d
 
-class Propagator(velocity.VelocityModel):
+class Propagator(seispy.velocity.VelocityModel):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        #self.regularize(2 * self.nodes["nr"],
         self.regularize(self.nodes["nr"],
                         self.nodes["ntheta"],
                         self.nodes["nphi"])
         self.pad(depth=30)
         self.pgrid = self.fit_propagation_grid()
         self.vgrids = np.array(
-                [np.stack((np.fliplr(np.flipud(np.copy(self.values["Vp"]))),
-                 np.fliplr(np.flipud(np.copy(self.values["Vs"])))))]
+                [np.stack((np.fliplr(np.copy(self.values["Vp"])),
+                           np.fliplr(np.copy(self.values["Vs"]))))]
                               )
         fm3d.initialize_propagation_grid(**self.pgrid)
         fm3d.initialize_velocity_grids(self.vgrids,
@@ -105,29 +103,63 @@ class Propagator(velocity.VelocityModel):
         R, T, P = np.meshgrid(r_nodes, t_nodes, p_nodes, indexing="ij")
         self.regrid(R, T, P)
 
+def write_sources(sources):
+    outf = open("/Users/malcolcw/Desktop/rays/sources.rtp", "w")
+    for sx in sources:
+        r, t, p = seispy.geometry.geo2sph(*sx)
+        outf.write("{:.6f} {:.6f} {:.6f}\n".format(r, t, p))
+    outf.close()
+
+def write_receivers(receivers):
+    outf = open("/Users/malcolcw/Desktop/rays/receivers.rtp", "w")
+    for rx in receivers:
+        r, t, p = seispy.geometry.geo2sph(*rx)
+        outf.write("{:.6f} {:.6f} {:.6f}\n".format(r, t, p))
+    outf.close()
+
+def write_rays(rays):
+    outfP = open("/Users/malcolcw/Desktop/rays/rays-P.rtp", "w")
+    outfS = open("/Users/malcolcw/Desktop/rays/rays-S.rtp", "w")
+    outfP.write("{:d}\n".format(rays.shape[0]*rays.shape[1]))
+    outfS.write("{:d}\n".format(rays.shape[0]*rays.shape[1]))
+    for sx in rays:
+        for rayP, rayS in sx:
+            outfP.write("{:d}\n".format(len([px for px in rayP if not np.any(px.mask)])))
+            outfS.write("{:d}\n".format(len([px for px in rayS if not np.any(px.mask)])))
+            rayP[:,1] *= -1
+            rayP[:,1] += np.pi / 2
+            rayS[:,1] *= -1
+            rayS[:,1] += np.pi / 2
+            for px in np.array([px for px in rayP if not np.any(px.mask)]):
+                outfP.write("{:.6f} {:.6f} {:.6f}\n".format(*px))
+            for px in np.array([px for px in rayS if not np.any(px.mask)]):
+                outfS.write("{:.6f} {:.6f} {:.6f}\n".format(*px))
+    outfP.close()
+    outfS.close()
+
 if __name__ == "__main__":
     prop = Propagator("/Users/malcolcw/Projects/Shared/Velocity/FANG2016/original/VpVs.dat", "fang", topo=seispy.topography.Topography("/Users/malcolcw/Projects/Shared/Topography/anza.xyz"))
-    #sources = np.array([[33., 242., 5.0],
-    #                    [33., 242.5, 10.],
-    #                    [33., 243., 15.],
-    #                    [33., 243.5, 19.]])
-    #receivers = np.array([[33., 242., 0.0],
-    #                      [33., 242.5, 0.0],
-    #                      [33., 243, 0.0],
-    #                      [33., 243.5, 0.0]])
-    sources = np.array([[33., 242., 5.0]])
-    receivers = np.array([[33., 242.9, 0.0]])
+    #prop = Propagator("/Users/malcolcw/Desktop/IASP91.fang.txt", "fang", topo=seispy.topography.Topography("/Users/malcolcw/Projects/Shared/Topography/anza.xyz"))
+    latmin = np.degrees(np.pi / 2 - prop.nodes["theta"][0,-2,0])
+    latmax = np.degrees(np.pi / 2 - prop.nodes["theta"][0,1,0])
+    lonmin = np.degrees(prop.nodes["phi"][0,0,1])
+    lonmax = np.degrees(prop.nodes["phi"][0,0,-2])
+    dlat = (latmax - latmin)
+    dlon = (lonmax - lonmin)
+    lat0 = np.mean([latmin, latmax])
+    lon0 = np.mean([lonmin, lonmax])
+
+    sources = np.array([[lat0 - 0.25*dlat, lon0 - 0.25*dlon, 15]])
+    receivers = np.empty((4,3))
+    receivers[:,:2] = [[lat0 - 0.25*dlat, lon0 - 0.25*dlon],
+                       [lat0 + 0.25*dlat, lon0 - 0.25*dlon],
+                       [lat0 + 0.25*dlat, lon0 + 0.25*dlon],
+                       [lat0 - 0.25*dlat, lon0 + 0.25*dlon]]
+
+    receivers[:,2] = np.array([prop.topo(*receivers[i,:2]) - seispy.constants.EARTH_RADIUS
+                                for i in range(len(receivers))])
     rays, tts = prop(sources, receivers)
-
-    import matplotlib.pyplot as plt
-    from mpl_toolkits.mplot3d import Axes3D
-    fig = plt.figure()
-    ax = fig.add_subplot(1, 1, 1, projection="3d")
-    rays[:,:,1,:,1] *= -1
-    rays[:,:,1,:,1] += np.pi / 2
-    ray = np.array([seispy.geometry.sph2xyz(*r) for r in rays[0,0,1] if not np.any(r.mask)])
-    ax.plot(ray[:,0], ray[:,1], ray[:,2])
-    ax.set_xlabel("X")
-    ax.set_ylabel("Y")
-    plt.show()
-
+    #plot(rays)
+    write_rays(rays)
+    write_sources(sources)
+    write_receivers(receivers)
