d698e2badc37cd9ce8adc174745e90211d1c2ecf
Malcolm White <malcolcw@usc.edu>
2017-10-06 17:01:57 -0700

    Stop using np.matrix class to do 3D rotations, remove some more Greek characters.

--- seispy/coords.py
+++ seispy/coords.py
@@ -4,7 +4,7 @@ This module deprecates the geometry module.
 import numpy as np
 import seispy
 
-π = np.pi
+pi = np.pi
 
 class GeographicCoordinates(np.ndarray):
     def __init__(self, args):
@@ -33,7 +33,7 @@ class GeographicCoordinates(np.ndarray):
     def to_cartesian(self):
         cart = CartesianCoordinates(self.shape[:-1])
         rho = seispy.constants.EARTH_RADIUS - self[...,2]
-        theta = π/2 - np.radians(self[...,0])
+        theta = pi/2 - np.radians(self[...,0])
         phi = np.radians(self[...,1])
         cart[...,0] = rho * np.sin(theta) * np.cos(phi)
         cart[...,1] = rho * np.sin(theta) * np.sin(phi)
@@ -68,13 +68,13 @@ class CartesianCoordinates(np.ndarray):
         the z-axis, then beta radians about the y'-axis and then
         gamma radians about the z''-axis.
         """
-        ALPHA = np.matrix([[np.cos(alpha), -np.sin(alpha), 0],
+        ALPHA = np.array([[np.cos(alpha), -np.sin(alpha), 0],
                            [np.sin(alpha), np.cos(alpha), 0],
                            [0, 0, 1]])
-        BETA = np.matrix([[np.cos(beta), 0, np.sin(beta)],
+        BETA = np.array([[np.cos(beta), 0, np.sin(beta)],
                           [0, 1, 0],
                           [-np.sin(beta), 0, np.cos(beta)]])
-        GAMMA = np.matrix([[np.cos(gamma), -np.sin(gamma), 0],
+        GAMMA = np.array([[np.cos(gamma), -np.sin(gamma), 0],
                            [np.sin(gamma), np.cos(gamma), 0],
                            [0, 0, 1]])
         R = ALPHA.dot(BETA).dot(GAMMA)
@@ -84,7 +84,7 @@ class CartesianCoordinates(np.ndarray):
     def to_geographic(self):
         geo = GeographicCoordinates(self.shape[:-1])
         rho = np.sqrt(np.sum(np.square(self),axis=-1))
-        geo[...,0] = np.degrees(π/2 - np.arccos(self[...,2]/rho))
+        geo[...,0] = np.degrees(pi/2 - np.arccos(self[...,2]/rho))
         geo[...,1] = np.degrees(np.arctan2(self[...,1], self[...,0]))
         geo[...,2] = seispy.constants.EARTH_RADIUS - rho
         return(geo)
@@ -92,7 +92,7 @@ class CartesianCoordinates(np.ndarray):
     def to_left_spherical(self):
         lspher = LeftSphericalCoordinates(self.shape[:-1])
         lspher[...,0] = np.sqrt(np.sum(np.square(self),axis=-1))
-        lspher[...,1] = π/2 - np.arccos(self[...,2]/lspher[...,0])
+        lspher[...,1] = pi/2 - np.arccos(self[...,2]/lspher[...,0])
         lspher[...,2] = np.arctan2(self[...,1], self[...,0])
         return(lspher)
 
@@ -117,9 +117,9 @@ class SphericalCoordinates(np.ndarray):
         super().__setitem__(index, coordinates)
         if False in [rho >= 0 for rho in self[...,0].flatten()]:
             raise(ValueError("all values for rho must satisfiy 0 <= rho"))
-        if False in  [0 <= theta <= π for theta in self[...,1].flatten()]:
-            raise(ValueError("all values for theta must satisfiy 0 <= theta <= π"))
-        if False in  [-π <= phi <= π for phi in self[...,2].flatten()]:
+        if False in  [0 <= theta <= pi for theta in self[...,1].flatten()]:
+            raise(ValueError("all values for theta must satisfiy 0 <= theta <= pi"))
+        if False in  [-pi <= phi <= pi for phi in self[...,2].flatten()]:
             raise(ValueError("all values for phi must satisfiy -π <= phi <= π"))
 
     def to_cartesian(self):
@@ -131,7 +131,7 @@ class SphericalCoordinates(np.ndarray):
 
     def to_geographic(self):
         geo = GeographicCoordinates(self.shape[:-1])
-        geo[...,0] = np.degrees(π/2 - self[...,1])
+        geo[...,0] = np.degrees(pi/2 - self[...,1])
         geo[...,1] = np.degrees(self[...,2])
         geo[...,2] = seispy.constants.EARTH_RADIUS - self[...,0]
         return(geo)
@@ -139,7 +139,7 @@ class SphericalCoordinates(np.ndarray):
     def to_left_spherical(self):
         lspher = LeftSphericalCoordinates(self.shape[:-1])
         lspher[...,0] = self[...,0]
-        lspher[...,1] = π/2  - self[...,1]
+        lspher[...,1] = pi/2  - self[...,1]
         lspher[...,2] = self[...,2]
         return(lspher)
 
@@ -157,16 +157,16 @@ class  LeftSphericalCoordinates(np.ndarray):
         super().__setitem__(index, coordinates)
         if False in  [rho >= 0 for rho in self[...,0].flatten()]:
             raise(ValueError("all values for rho must satisfiy 0 <= rho"))
-        if False in  [-π/2 <= theta <= π/2 for theta in self[...,1].flatten()]:
+        if False in  [-pi/2 <= theta <= pi/2 for theta in self[...,1].flatten()]:
             raise(ValueError("all values for lambda must satisfiy -π/2 <= lambda <= π/2"))
-        if False in  [-π <= phi <= π for phi in self[...,2].flatten()]:
+        if False in  [-pi <= phi <= pi for phi in self[...,2].flatten()]:
             raise(ValueError("all values for phi must satisfiy -π <= phi <= π"))
 
     def to_cartesian(self):
         cart = CartesianCoordinates(self.shape[:-1])
-        cart[...,0] = self[...,0]*np.sin(π/2 - self[...,1])*np.cos(self[...,2])
-        cart[...,1] = self[...,0]*np.sin(π/2 - self[...,1])*np.sin(self[...,2])
-        cart[...,2] = self[...,0]*np.cos(π/2 - self[...,1])
+        cart[...,0] = self[...,0]*np.sin(pi/2 - self[...,1])*np.cos(self[...,2])
+        cart[...,1] = self[...,0]*np.sin(pi/2 - self[...,1])*np.sin(self[...,2])
+        cart[...,2] = self[...,0]*np.cos(pi/2 - self[...,1])
         return(cart)
 
     def to_geographic(self):
@@ -179,7 +179,7 @@ class  LeftSphericalCoordinates(np.ndarray):
     def to_spherical(self):
         spher = SphericalCoordinates(self.shape[:-1])
         spher[...,0] = self[...,0]
-        spher[...,1] = π/2 - self[...,1]
+        spher[...,1] = pi/2 - self[...,1]
         spher[...,2] = self[...,2]
         return(spher)
 
