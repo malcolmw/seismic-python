b0d7eb17de8f8935511808f56ffb548c1853d2ed
Malcolm White <malcolcw@usc.edu>
2018-08-24 22:09:58 -0700

    UPDATE:: Use "from . import" syntax.

---
+++ seispy/core/__init__.py
@@ -0,0 +1,18 @@
+# coding=utf-8
+
+from . import constants
+from . import coords
+from . import faults
+from . import fmm3dio
+from . import geogrid
+from . import geometry
+try:
+    from . import mapping
+except ImportError:
+    print("seispy.core.mapping could not be imported, probably due to "
+          "missing mpl_toolkits.basemap module.")
+from . import stats
+from . import surface
+from . import topography
+from . import ttgrid
+from . import velocity
