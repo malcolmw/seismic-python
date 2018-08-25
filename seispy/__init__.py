309a23bd3886b96d99ffa449b620dd951b47773d
Malcolm White <malcolcw@usc.edu>
2018-08-24 22:09:23 -0700

    UPDATE:: Import everything from core module into root namespace.

--- seispy/__init__.py
+++ seispy/__init__.py
@@ -1,15 +1,2 @@
-# coding=utf-8
-
-from . import constants
-from . import coords
-from . import faults
-from . import fmm3dio
-from . import geogrid
-from . import geometry
-from . import mapping
-from . import stats
-from . import surface
-from . import topography
-from . import ttgrid
-from . import velocity
+from .core import *
 from . import pandas
