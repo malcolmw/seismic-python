af7d70fa44bb71fab02b29cfec20ec48b98ca3a3
Malcolm White <malcolm.white@usc.edu>
2019-08-03 12:44:44 -0400

    DEBUG:: Debug I/O capabilities for Hypoinverse2000 phase data from NCEDC

--- seispy/pandas/io/special.py
+++ seispy/pandas/io/special.py
@@ -5,25 +5,7 @@ import pickle
 import pkg_resources
 from . import schema as _schema
 
-def _index_origin_rows(fname):
-    """
-    Return the total number of lines and indices of origin
-    rows in input file.
-
-    This is a utility function for parsing hypoinverse2000
-    phase format data.
-    """
-    with open(fname) as inf:
-        data = inf.read().rstrip("\n").split("\n")
-    nrows = len(data)
-    # The variable lengths (164, 179) of origin rows is to account for NCEDC
-    # format data which is slightly different than SCEDC data.
-    return (
-        nrows,
-        np.array([i for i in range(len(data)) if len(data[i]) in (164, 179)])
-    )
-
-def read_special(path, schema="hypoinverse2000", tables=None, ncedc=False):
+def read_special(path, schema="hypoinverse2000", tables=None):
     """
     Read database tables into a Pandas DataFrame. This function
     is for reading formats that don't lend themselves to more
@@ -49,13 +31,32 @@ def read_special(path, schema="hypoinverse2000", tables=None, ncedc=False):
     """
 
     if schema == "hypoinverse2000":
-        return(_read_hypoinverse2000(path, ncedc=ncedc))
+        return(_read_hypoinverse2000(path))
     else:
         raise(NotImplementedError("schema not recognized: %s" % schema))
 
-def _read_hypoinverse2000(path, ncedc=False):
+def _index_rows(fname):
+    """
+    Return the total number of lines and indices of origin
+    rows in input file.
+
+    This is a utility function for parsing hypoinverse2000
+    phase format data.
+    """
+    with open(fname) as inf:
+        data = inf.read().rstrip("\n").split("\n")
+    nrows = len(data)
+    # The variable lengths (164, 179) of origin rows is to account for NCEDC
+    # format data which is slightly different than SCEDC data.
+    return (
+        nrows,
+        np.array([i for i in range(len(data)) if len(data[i]) in (164, 179)]),
+        np.array([i for i in range(len(data)) if len(data[i]) == 120])
+    )
+
+def _read_hypoinverse2000(path):
     schema_data = _schema.get_schema("hypoinverse2000")
-    nrows, origin_rows = _index_origin_rows(path)
+    nrows, origin_rows, arrival_rows = _index_rows(path)
     db = {}
     db["origin"] = pd.read_fwf(
         path,
@@ -74,14 +75,19 @@ def _read_hypoinverse2000(path, ncedc=False):
             for field in schema_data["Relations"]["arrival"]
         ],
         names=schema_data["Relations"]["arrival"],
-        skiprows=origin_rows,
+        skiprows=[i for i in range(nrows) if i not in arrival_rows],
         header=None
     )
-    origin_rows = np.append(origin_rows, nrows)
-    for idx in range(len(origin_rows)-1):
-        start = origin_rows[idx] - idx
-        stop = origin_rows[idx+1] - (idx + 2)
-        db["arrival"].loc[start:stop, "evid"] = db["origin"].loc[idx, "evid"]
+    start = 0
+    for iorigin in range(len(origin_rows)-1):
+        stop = start + len(
+            arrival_rows[
+                 (arrival_rows > origin_rows[iorigin])
+                &(arrival_rows < origin_rows[iorigin+1])
+            ]
+        )
+        db['arrival'].loc[start: start+nrows, 'evid'] = db['origin'].loc[iorigin, 'evid']
+        start = stop
     for table in db:
         db[table] = db[table].fillna(
             value={
@@ -90,14 +96,14 @@ def _read_hypoinverse2000(path, ncedc=False):
             }
         )
         db[table] = db[table].astype(
-            {
-                field: schema_data["Attributes"][field]["dtype"]
-                for field in db[table].columns
-            }
+           {
+               field: schema_data["Attributes"][field]["dtype"]
+               for field in db[table].columns
+           }
         )
         for field in db[table].columns:
             if "const" in schema_data["Attributes"][field]:
                 db[table][field] = db[table][field] \
                     * schema_data["Attributes"][field]["const"]
     db["arrival"] = db["arrival"].drop(columns=["blank1", "blank2", "blank3"])
-    return(db)
+    return (db)
